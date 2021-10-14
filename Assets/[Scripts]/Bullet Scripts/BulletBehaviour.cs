510c836c5a0901c6bef1e14ca6ffbbd303c3dbd4
Mingkun Yang <ymk1234567890ymk@sina.com>
2021-10-14 04:55:55 -0400

    Leb 4 submission

---
+++ Assets/[Scripts]/Bullet Scripts/BulletBehaviour.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    public BulletType type;
+
+    [Header("Bullet Movement")]
+    [Range(0.0f, 0.5f)]
+    public float speed;
+    public Bounds bulletBounds;
+    public BulletDirection direction;
+
+    private BulletManager bulletManager;
+    private Vector3 bulletVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletManager = GameObject.FindObjectOfType<BulletManager>();
+
+        switch(direction)
+        {
+            case BulletDirection.UP:
+                bulletVelocity = new Vector3(0.0f, speed, 0.0f);
+                break;
+            case BulletDirection.RIGHT:
+                bulletVelocity = new Vector3(0.0f, 0.0f, 0.0f);
+                break;
+            case BulletDirection.DOWN:
+                bulletVelocity = new Vector3(0.0f, -speed, 0.0f);
+                break;
+            case BulletDirection.LEFT:
+                bulletVelocity = new Vector3(-speed, 0.0f, 0.0f);
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+        CheckBounds();
+    }
+
+    private void Move()
+    {
+        transform.position += bulletVelocity;
+    }
+
+    private void CheckBounds()
+    {
+        //check bottom bounds
+        if (transform.position.y < bulletBounds.max)
+        {
+            //Destroy(this.gameObject);
+
+            bulletManager.ReturnBullet(this.gameObject, type);
+        }
+
+        //check top bounds
+        if (transform.position.y > bulletBounds.min)
+        {
+            //Destroy(this.gameObject);
+
+            bulletManager.ReturnBullet(this.gameObject, type);
+        }
+    }  
+}
---
+++ Assets/[Scripts]/Bullet Scripts/BulletDirection.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public enum BulletDirection 
+{ 
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+}
+
